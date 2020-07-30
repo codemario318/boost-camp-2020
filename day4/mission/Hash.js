3b04669e5e4957b20091d21a5a08637c73f9c83b
LEE.DH <dhlee318@me.com>
2020-07-30 17:27:45 +0900

    코드값의 합을 이용한 해시함수 구현

--- day4/mission/Hash.js
+++ day4/mission/Hash.js
@@ -1,11 +1,22 @@
 class Hash {
-    constructor (BucketSize) {
-
+    constructor (maxSize) {
+        this.maxSize = maxSize; 
     }
 
-    getHashCode() {
+    getHashCode(key) {
+        const keyString = String(key);
+        let code_sum = 0;
 
+        for (let i = 0; i < keyString.length; i++) {
+            code_sum += keyString.charCodeAt(i);
+        }
+        return code_sum % this.maxSize;
     }
 }
 
 module.exports = Hash;
+
+// const hash = new Hash(10);
+// for (let i = 0; i < 20; i++) {
+//     console.log(hash.getHashCode(i));
+// }
