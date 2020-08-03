85747d6fb7acec3f3942f29ea8e9a114b8978627
LEE.DH <dhlee318@me.com>
2020-08-03 13:38:28 +0900

    Point class 최대 최소값 추가

--- day6/Point.js
+++ day6/Point.js
@@ -1,6 +1,8 @@
 module.exports = class Point {
-    constructor(x, y) {
+    constructor(x, y, min=0, max=24) {
         this.x = x;
         this.y = y;
+        this.min = min;
+        this.max = max;
     }
 }
